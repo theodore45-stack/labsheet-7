515bf200c850a7e00f0a23c8485b43bc9a5875d4
Sahil Kumar <sahil7904kumar@gmail.com>
2025-11-09 23:01:22 +0530

    Copy only first 5 characters

---
+++ 18.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char src[100], dest[100];
+    cout << "Enter a string: ";
+    cin.getline(src, 100);
+    strncpy(dest, src, 5);
+    dest[5] = '\0';
+    cout << "Copied (first 5 chars): " << dest;
+    return 0;
+}
