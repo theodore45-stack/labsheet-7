9257dcef5f6917bccaf0e54d1a32eccb23d69869
Sahil Kumar <sahil7904kumar@gmail.com>
2025-11-09 23:00:56 +0530

    Remove all vowels from a string

---
+++ 17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[200], result[200];
+    int j = 0;
+    cout << "Enter a string: ";
+    cin.getline(str, 200);
+
+    for (int i = 0; i < strlen(str); i++) {
+        char c = tolower(str[i]);
+        if (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'))
+            result[j++] = str[i];
+    }
+    result[j] = '\0';
+    cout << "String without vowels: " << result;
+    return 0;
+}
